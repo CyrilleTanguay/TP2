edc84ff9989e1ac38b7d6d46385f5c385bc37289
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2021-05-16 15:14:06 -0400

    Changement de la taille des miniatures

--- template-parts/content-galerie.php
+++ template-parts/content-galerie.php
@@ -9,7 +9,7 @@
 <article class="flip-card">
 <div class="flip-card-inner">
 <div class="flip-card-front">
-<?php the_post_thumbnail('thumbnail');?>
+<?php the_post_thumbnail(array(300,300));?>
 </div>
 <div class="flip-card-back">
 <h1><a href="<?php echo get_permalink();?>"><?php the_title(); ?></a></h1>
